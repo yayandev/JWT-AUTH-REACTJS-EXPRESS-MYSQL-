2b0a7baeaea70b518960ec93cb075b9692f535d9
YANZ <107906629+Yayanfr20@users.noreply.github.com>
2023-06-02 20:57:17 +0700

    UPP BACKEDN

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import db from "./config/Database.js";
+import router from "./routes/index.js";
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import cors from "cors"
+dotenv.config()
+const app = express();
+try {
+  await db.authenticate();
+  console.log("Database conenected...");
+} catch (error) {
+  console.error(error);
+}
+
+app.use(cors({ credentials:true, origin:'http://localhost:3000' }))
+app.use(cookieParser());
+app.use(express.json());
+app.use(router);
+
+app.listen(5000, () => console.log("Server running at port 5000"));
